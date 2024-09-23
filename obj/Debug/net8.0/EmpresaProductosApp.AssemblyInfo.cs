735a94b9af9d829e9b76234cb6e4d0a5399e3edf
Santiago Buitrago <jsrojasbuitrago@gmail.com>
2024-09-23 01:56:53 -0500

    Updated

--- obj/Debug/net8.0/EmpresaProductosApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmpresaProductosApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpresaProductosApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+928c1374bd41817ba51679d3f7ea17442f4531a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpresaProductosApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpresaProductosApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
